c95c9fd2c9305bf9d85a2ef509464bec81c27a2a
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-12-18 23:20:31 +0000

    Import pandas in DataTransform.date_convert method.

--- DataTransform.py
+++ DataTransform.py
@@ -3,9 +3,11 @@ class DataTransform:
        We have added methods to the DataTransform class which we can apply to the DataFrame columns to perform any required conversions.
     '''
     def ___init___(self):
-        import pandas as pd
+        pass
 
     def date_convert(self, data):
+        import pandas as pd
+
         data['issue_date'] = pd.to_datetime(data['issue_date'])
         data['last_payment_date'] = pd.to_datetime(data['last_payment_date'])
         data['next_payment_date'] = pd.to_datetime(data['next_payment_date'])
