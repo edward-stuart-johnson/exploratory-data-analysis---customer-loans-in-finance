b892b248dc7eb0a34a5ac3147c57da7076100f3c
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-12-18 20:58:17 +0000

    Clean-up and refactor credentials_loader.

--- credentials_loader.py
+++ credentials_loader.py
@@ -1,15 +1,12 @@
-
-
-# A function which loads the credentials.yaml file and returns the data dictionary contained within. 
-
-# This will be be passed to your RDSDatabaseConnector as an argument which the class will use to connect to the remote database.
 def credentials_loader():
+	# A function which loads the credentials.yaml file and returns the data dictionary contained within. 
+	# This will be be passed to  RDSDatabaseConnector as an argument which the class will use to connect to the remote database.
 	import yaml
 	from pathlib import Path
+
 	filepath = Path(__file__).with_name("credentials.yaml")
 	print(filepath)
 	with filepath.open("r") as f:
-	# with open("credentials.yaml", "r") as f:
 		credentials_dictionary = yaml.safe_load(f)
 	print(credentials_dictionary)
 	return credentials_dictionary
