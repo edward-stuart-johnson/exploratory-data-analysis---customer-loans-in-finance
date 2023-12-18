c3e5918bd7e3c34524a179757166d4e4667aeb74
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-12-18 20:56:26 +0000

    Clean up code - delete uneccessary code.

--- db_utils.py
+++ db_utils.py
@@ -1,24 +1,20 @@
-# from winreg import ConnectRegistry
 
 
 class RDSDatabaseConnector:
 	def ___init___(self, credentials_dictionary):
 		# initialises
-		# from credentials_loader import credentials_loader
-		# credentials_loader()
 
 		loan_payments = False
 		
 	def _SQLAlchemy_initialiser_(self):
-		# initialises a SQLAlchemy engine from the credentials provided to your class. 
-		# This engine object together with the Pandas library will allow you to extract data from the database. 
+		# initialises a SQLAlchemy engine from the credentials provided.
+		# This engine object together with the Pandas library will extract data from the database. 
 
 		from credentials_loader import credentials_loader
 		credentials_dictionary = credentials_loader()
 		print("Credentials dictionary is: ",credentials_dictionary)
 
 		import psycopg2
-
 		import sqlalchemy
 		from sqlalchemy import create_engine
 		from sqlalchemy.orm import sessionmaker
@@ -27,60 +23,25 @@ class RDSDatabaseConnector:
 		print("Engine configuration is: ",engine_configuration)
 		
 		engine = create_engine(engine_configuration)
-
 		engine.execution_options(isolation_level='AUTOCOMMIT').connect()
 
 		return engine
 
-		# from sqlalchemy import inspect
-		# inspector = inspect(engine)
-		# inspector.get_table_names()
-
-		# from sqlachemy import text
-		# with engine.connect() as connection:
-		# 	result = connection.execute(text("SELECT * "))
-		# 	for row in result:
-		# 		print(row)
-
-		# # # # # # # # # # # # # # # # # # # # # # # # 
-        
-		# meta = MetaData(engine)
-		# my_table = Table(
-		# 'my_table',
-		# meta,
-		# autoload=True,
-		# schema=db_name
-		# )
-
-		# dbsession = sessionmaker(bind=engine)
-		# session = dbsession()
-
-		# # example query to table
-		# results = session.query(my_table).filter(my_table.columns.id >=1)
-		# results.all()
-
 	def _RDS_data_extractor_(self, engine):
-		# Develop a method which extracts data from the RDS database and returns it as a Pandas DataFrame. 
+		# Extracts data from the RDS database and returns it as a Pandas DataFrame. 
 		# The data is stored in a table called loan_payments. 
-		# example query to table
+
 		import pandas as pd
 
-		# self.SQLAlchemy_initialiser()
-
-		# with self.SQLAlchemy_initialiser.engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
 		self.loan_payments = pd.read_sql_table('loan_payments', engine)
 		self.loan_payments.head(10)
 		print(self.loan_payments)
-		# pd.df(session.query(my_table).filter(my_table.columns.id >=1))
 		return self.loan_payments
 
 	
 	def _saver_(self, loan_payments):
-		# saves the data to an appropriate file format to your local machine. 
-		# This should speed up loading up the data when you're performing your EDA/analysis tasks. 
-		# The function should save the data in .csv format, since we're dealing with tabular data.
-
-		# self.RDS_data_extractor()
+		# Saves the tabular data in an appropriate .csv  format to local machine. 
+		# This should speed up loading up the data when performing  EDA/analysis tasks. 
 
 		import pandas as pd
 
