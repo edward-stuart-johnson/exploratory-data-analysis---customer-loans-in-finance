a627fcc0689c6bb9ad7cdfa7a97b8dad4c8c8f9c
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-12-22 01:23:04 +0000

    Create DataFrameInfo.py with specification inside.

---
+++ DataFrameInfo.py
@@ -0,0 +1,13 @@
+# After converting your columns to a more appropriate format you may want to develop a class to extract information from the DataFrame and its columns. Create a DataFrameInfo class which will contain methods that generate useful information about your DataFrame.
+
+# Some useful utility methods you might want to create that are often used for EDA tasks are:
+
+#     Describe all columns in the DataFrame to check their data types
+#     Extract statistical values: median, standard deviation and mean from the columns and the DataFrame
+#     Count distinct values in categorical columns
+#     Print out the shape of the DataFrame
+#     Generate a count/percentage count of NULL values in each column
+#     Any other methods you may find useful
+
+# Creating a class like this will be useful in the future since these are common tasks for performing EDA on any dataset.
+
