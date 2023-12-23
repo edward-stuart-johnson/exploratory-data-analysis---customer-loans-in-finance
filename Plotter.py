7533d84e246770e83d2ef4090b9d029ba22a8036
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-12-23 22:10:02 +0000

    Add sns.displot to visualise proportions of null values.

--- Plotter.py
+++ Plotter.py
@@ -8,9 +8,20 @@ class Plotter:
         import seaborn as sns
         import matplotlib.pyplot as plt
         # heatmap
-        plt.figure(figsize=(10,6))
+        # plt.figure(figsize=(10,6)) # iS THSI OPTIONAL? CAN I MAKE IT AUTOFIT ALL COLUMNNAMES?
         sns.heatmap(df.isna().transpose(),
                     cmap='YlGnBu',
                     cbar_kws={'label':'Missing Data'})
         plt.savefig('visualing_null_values.png',
                     dpi=100)
+        sns.displot(
+            data=df.isna().melt(value_name='missing'),
+                                y='variable',
+                                hue='missing',
+                                multiple='fill',
+                                aspect=1.25
+            )
+        plt.savefig('visualising_missing_data.png',
+                        dpi=100)
+        
+        
