12a42d55776ecd36eb9b5482383a492058ec4e59
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-12-23 02:34:41 +0000

    Create a method in Plotter class to visualise the removal of null values.

--- Plotter.py
+++ Plotter.py
@@ -2,3 +2,15 @@ class Plotter:
     # a class to visualise insights from the data
     def __init__(self) -> None:
         pass
+    def visualise_null_values(self, df):
+        '''This method visualises the removal of null values.'''
+        import pandas as pd
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        # heatmap
+        plt.figure(figsize=(10,6))
+        sns.heatmap(df.isna().transpose(),
+                    cmap='YlGnBu',
+                    cbar_kws={'label':'Missing Data'})
+        plt.savefig('visualing_null_values.png',
+                    dpi=100)
